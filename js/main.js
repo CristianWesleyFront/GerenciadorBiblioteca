51a7e0e5abee77e146459a6cb2e92e38bbd0d590
Cristian Wesley Correia De Oliveira <F1811101@for.lanlink.com.br>
2018-11-27 19:29:38 -0200

    Teste Git

---
+++ js/main.js
@@ -0,0 +1,57 @@
+/**
+ * {
+ *   id: Number --> É um valor obrigatório e incrementál
+ *   nome: String --> É um valor obrigatório e não pode conter menos que 3 caracteres
+ *   autor: String/undefined --> Não é um valor obrigatório
+ *   isbn: Isbn --> É um valor obrigatório e deve ser sempre válido.
+ * }: Object
+ */
+/** 
+Dicas para uma boa pontuação:
+
+1. Utilize ao máximo métodos de array
+2. Utilize funções reutilizáveis (se você escrever a mesma coisa mais de uma vez levara penalidade)
+3. Boa documentação
+
+OBS: não precisa de HTML e CSS :)
+*/
+
+
+var livros = [
+    {
+      id: 1,
+      nome: 'Como as democracias morrem',
+      autor: 'Steven Levitsky',
+      isbn: '9788537818008'
+    },
+    {
+      id: 2,
+      nome: 'Fascismo: Um alerta',
+      autor: 'Madeleine Albright',
+      isbn: '8542214277'
+    },
+    {
+      id: 3,
+      nome: 'Ruptura: A crise da democracia liberal',
+      autor: 'Manuel Castells',
+      isbn: '8537817643'
+    }
+  ];
+  
+  var actualId = 4;
+  
+  var adicionarLivro = function(livro) {
+  
+  }
+  
+  var lerLivro = function(idLivro) {
+  
+  }
+  
+  var atualizarLivro = function(idLivro, novoLivro) {
+  
+  }
+  
+  var removerLivro = function(idLivro) {
+  
+  }
