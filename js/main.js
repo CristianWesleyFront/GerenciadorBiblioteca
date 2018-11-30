4a5843be18d90ad101c40bafb0aca31dbc55dfd5
Cristian Wesley Correia De Oliveira <F1811101@for.lanlink.com.br>
2018-11-30 19:46:30 -0200

    Adicionado novas funçoes da atvidade 2, LivrosMaisVotados,adicionar voto, novo remover livro e ler livro

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-let livros = [
-  { id: 1,nome: 'Como as democracias morrem',autor: 'Steven Levitsky',isbn: '9788537818008' },
-  { id: 2, nome: 'Fascismo: Um alerta', autor: 'Madeleine Albright', isbn: '8542214277'},
-  { id: 3, nome: 'Ruptura: A crise da democracia liberal', autor: 'Manuel Castells', isbn: '8537817643'}
-];
-
 function verificarIsbn10Dig(isbn){
   let total = 0;
 
@@ -103,6 +97,145 @@ function tratarIndex(idLivro){
  return index;
 };
 
+let livros = [
+  { id: 1,nome: 'Como as democracias morrem',autor: 'Steven Levitsky',isbn: '9788537818008' },
+  { id: 2, nome: 'Fascismo: Um alerta', autor: 'Madeleine Albright', isbn: '8542214277'},
+  { id: 3, nome: 'Ruptura: A crise da democracia liberal', autor: 'Manuel Castells', isbn: '8537817643'},
+  { id: 4, nome: "w", autor: "a3" },
+  { id: 5, nome: "k", autor: "a4" },
+  { id: 6, nome: "h", autor: "a5" }
+];
+
+let votosLivros = [
+  { idLivro: 4, voto: 4.0 },
+  { idLivro: 6, voto: 2.8 },
+  { idLivro: 2, voto: 4.5 },
+  { idLivro: 2, voto: 5.0 },
+  { idLivro: 2, voto: 1.1 },
+  { idLivro: 1, voto: 0.5 },
+  { idLivro: 3, voto: 0.7 },
+  { idLivro: 4, voto: 2.8 },
+  { idLivro: 2, voto: 1.9 },
+ ];
+
+
+function LerVotos(idLivro){
+  let array = votosLivros.filter(function(votosLivros){
+    return votosLivros.idLivro == idLivro;
+  });
+  return array; 
+};
+function filtraValorVotos(idLivro){
+  let array = LerVotos(idLivro);
+  let arrayaux = [];
+  for(let i in array){
+    arrayaux[i] = array[i].voto;
+  }
+  return arrayaux;
+};
+function quantidadeVotos(idLivro){
+  let arrayEscolhidos = LerVotos(idLivro);
+  let quantidade = arrayEscolhidos.length;
+
+  return quantidade;
+};
+function mediaVotos(idLivro){
+  let arrayEscolhidos = filtraValorVotos(idLivro);
+  
+  let quantidade = quantidadeVotos(idLivro);
+  let total = 0;
+  for(let i in arrayEscolhidos){
+    total = total + arrayEscolhidos[i];
+  }
+  return total/quantidade;
+};
+function removerVotos(idLivro){
+  let array = votosLivros.filter(function(votoLivro){
+    return  votoLivro.idLivro != idLivro;
+  })
+  votosLivros = array;
+  //return votosLivros;
+};
+//---------------------------------
+function listaIdLivros(){
+  let arrayLivros = livros.map(function(livros){
+    return livros.id;
+  });
+  
+  return arrayLivros;
+};
+
+function listaQuantidadeVotos(){
+  let arrayLivrosId = listaIdLivros();
+  let quantidade = [];
+  for (let i in arrayLivrosId){
+    quantidade[i] = quantidadeVotos(arrayLivrosId[i]);
+  }
+  return quantidade;
+};
+function ordernaArrayDecrescente(){
+  let quantidadesVoto = FazObjetoIdVotos();
+  quantidadesVoto = quantidadesVoto.sort(function(a, b) {
+    //MITERIOS DE WELSON
+    if (a.numeroVotos > b.numeroVotos) {
+      return 1;
+    }
+
+    if (a.numeroVotos < b.numeroVotos) {
+      return -1;
+    }
+
+    return 0;
+  });
+  quantidadesVoto = quantidadesVoto.reverse();
+  
+  return quantidadesVoto;
+};
+
+function criaVinculoIdVotos(numeroVotos,idLivro){
+  return {
+    numeroVotos : numeroVotos,
+    id : idLivro
+  }
+};
+function FazObjetoIdVotos(){
+  let quantidadesVoto = listaQuantidadeVotos();
+  let arrayIdsLivros = listaIdLivros();
+  
+  let arrayMaior = [];
+
+  for (let i =0 ; i < quantidadesVoto.length; i++){
+    arrayMaior[i] = criaVinculoIdVotos(quantidadesVoto[i],arrayIdsLivros[i]);
+  }
+  
+  
+  return arrayMaior;
+};
+
+function buscaNomeLivro(idLivro){
+  let ler = livros.filter(function(livros){
+    return livros.id == idLivro;
+  });
+  return ler[0].nome;
+};
+//-----------------------------------
+function  listaAutores(){
+  let array = livros.map(function(livros){
+    return livros.autor;
+  });
+  return array;
+};
+
+function toFixedDoisDigitos(numero){
+  let valorX = numero;
+  
+  valorX = valorX.toFixed(1);
+  valorX = parseFloat(valorX);
+  return valorX;
+}
+//----------------------------------
+
+
 
 
 
@@ -138,15 +271,14 @@ const adicionarLivro = livro => {
 };
 
 const removerLivro = idLivro => {
-  let id = tratarIndex(idLivro);
-  if((livros.length) >= idLivro){
-    livros.splice(id,1)
-    return "Processo feito com sucesso!";
-  } 
-  return "livro não encontrado";
+  removerVotos(idLivro);
+  let array = livros.filter(function(livros){
+    return  livros.id != idLivro;
+  })
+  livros = array;
 };
 
-var atualizarLivro = (idLivro, novoLivro) => {
+const atualizarLivro = (idLivro, novoLivro) => {
   let livro = novoLivro;
   let erro = 0;
   let aux;
@@ -197,19 +329,53 @@ const lerLivro = idLivro => {
   let ler = livros.filter(function(livros){
     return livros.id == idLivro;
   });
+  
+  ler[0].mediaVotos = mediaVotos(idLivro);
+  ler[0].quantidadeVotos = quantidadeVotos(idLivro); 
+
   ler.length == 0 ? aux = 'Livro não encontrado': aux = ler;
   return aux;
 };
 
+const livrosMaisVotados = quantidadelivros =>{
+  if(quantidadelivros <= livros.length+1){
+    let mostraMaior=[];
+    let votosOrdenados = ordernaArrayDecrescente();
+    for (let i=0; i< quantidadelivros;i++){
+      mostraMaior[i] = "Nome: "+buscaNomeLivro(i+1)+", ID: "+votosOrdenados[i].id+", Numero de votos:"+votosOrdenados[i].numeroVotos;
+    }
+    
+    return mostraMaior;
+  }
+  return 'Id invalido, quantidade de livros superior a do banco de dados';
+};
+const autorMaisLivros = () =>{
+  
+};//FALTA FAZER
+
+const adicionarVoto = (idlivro,valorVoto) =>{
+  let valorVotoX = toFixedDoisDigitos(valorVoto);
+  let voto = {
+    idlivro: idlivro,
+    voto: valorVotoX
+  }
+  votosLivros.push(voto);
+};// FALTA MENSAGEM DE ERRO
 
 
 
-console.log(adicionarLivro({nome:'Crist',autor:'dapd', isbn:'8524023945'}));
-console.log(adicionarLivro({nome:'Crist',autor:'dapd', isbn:'8524023945'}));
-// console.log(atualizarLivro(3,{autor:'wesley'}));
-console.log(lerLivro(5));
-console.log(removerLivro(5));
-console.log(lerLivro(5));
-console.log(adicionarLivro({nome:'Crist',autor:'dapd', isbn:'8524023945'}));
-console.log(atualizarLivro(5 ,{ id: 27 }));
-console.log(lerLivro(5));
+
+
+
+//let array = filtraValorVotos(2);
+//let array = LerVotos(2);
+//let array = mediaVotos(2);
+//removerLivro(2);
+//console.log(livros);
+//console.log(votosLivros);
+//console.log(ordernaArrayDecrescente());
+//console.log(livrosMaisVotados(4));
+//console.log(lerLivro(2));
+//console.log(adicionarVoto(2,5));
+//console.log(votosLivros);
+console.log(toFixedDoisDigitos(2.52525));
